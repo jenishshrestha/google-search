4385fa8bb72b7ee2da60269dba6ebac0d87f5e57
jenishshrestha <jenishhrestha@gmail.com>
2023-01-31 15:14:18 +0545

    undefined formattedTotalResults fix

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,25 +6,25 @@ export default function SearchResults({ results }) {
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
       <p className="mt-3 mb-5 text-sm text-gray-600">
-        About {results.searchInformation.formattedTotalResults} results (
-        {results.searchInformation.formattedSearchTime} seconds)
+        About {results?.searchInformation?.formattedTotalResults} results (
+        {results?.searchInformation?.formattedSearchTime} seconds)
       </p>
       {results.items?.map((result) => (
         <div key={result.link} className="max-w-xl mb-8">
           <div className="group">
-            <a className="text-sm truncate" href={result.link}>
-              {result.formattedUrl}
+            <a className="text-sm truncate" href={result?.link}>
+              {result?.formattedUrl}
             </a>
             <a
               className="group-hover:underline decoration-blue-800"
-              href={result.link}
+              href={result?.link}
             >
               <h2 className="text-xl font-medium text-blue-800 truncate">
-                {result.title}
+                {result?.title}
               </h2>
             </a>
           </div>
-          <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+          <p className="text-gray-600">{Parser(result?.htmlSnippet)}</p>
         </div>
       ))}
 
